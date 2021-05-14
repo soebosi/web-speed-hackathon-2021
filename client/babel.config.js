a469af12df152a0a0b29bb38078880c8af28ac83
soebosi <isobe_soichiro@abema.tv>
2021-05-14 19:54:11 +0900

    めんどくてpreset-env消しちゃうけど、もしchromeで動かんやつあったら死ぬな

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,17 +1,9 @@
 module.exports = {
   presets: [
     [
-      '@babel/preset-env',
-      {
-        corejs: '3',
-        modules: 'commonjs',
-        useBuiltIns: false,
-      },
-    ],
-    [
       '@babel/preset-react',
       {
-        development: true,
+        development: false,
       },
     ],
   ],
